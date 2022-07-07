747b551275afdd5d0c41bcbcb8095fa72d7a7c09
Sandro <sandrobrasil@ymail.com>
2022-07-07 17:36:20 -0300

    adicionando um console log

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação se conectado ao banco de dados")
+// mais um console log de teste
+console.log("O Paulo fez esse commit sem voce ver na aula")
+console.log("Enviando para o GitHub")
